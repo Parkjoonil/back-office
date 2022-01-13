d5205badd694a1ef2d5fded89e5e690f5ec444ae
joonil <joonil@lepisode.team>
2022-01-13 16:10:24 +0900

    solve problem about shareid page

--- src/app/shareid/shareid-detail/shareid-detail.component.ts
+++ src/app/shareid/shareid-detail/shareid-detail.component.ts
@@ -18,50 +18,19 @@ interface Shareidid {
 })
 export class ShareidDetailComponent implements OnInit {
 
-  shareidids: Shareidid[] = [
-    {
-      head: '스마트스토어',
-      object: '스토어관리',
-      company: 'JRI 네이버 스마트스토어',
-      id: 'manwolfarm@naver.com',
-      password: '88782314p*',
-      reference: '-- --',
-      fax: '062 0154 1544',
-      url: 'http://wpsolar.cafe24.com/administrator'
-    },
-    {
-      head: '산돌구름',
-      object: '디자인-폰트',
-      company: '구름다리',
-      id: 'jusung@surhyun.com',
-      password: '88782314p*',
-      reference: '2022년 2월 9일 10시 40분 만료',
-      fax: '062 1544 1544',
-      url: 'http://wpsolar.cafe24.com/administrator'
-    },
-    {
-      head: '로젠택배',
-      object: '택배관리',
-      company: 'IROGEN',
-      id: '60150107',
-      password: '88782314p*',
-      reference: '-- --',
-      fax: '-- --',
-      url: '-- --'
-    }
-  ]
 
   @Input()
-  shareid: Shareidid[] =[{
+  shareids: Shareidid ={
     head: '',
     object: '',
     company: '',
     id: '',
     password: '',
-   reference: '',
+    reference: '',
     fax: '',
     url: ''
-  }]
+  }
+  
 
   constructor() { }
 
