b534e5ef80304d1646950b5ef9dfe704abcd8237
Parkjoonil <106866130+Parkjoonil@users.noreply.github.com>
2022-07-20 23:55:29 +0900

    project 페이지 수정

--- src/app/pages/project/project-detail/project-detail.component.ts
+++ src/app/pages/project/project-detail/project-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Project } from 'src/app/interfaces/Project.interface';
+import { ProjectService } from 'src/app/providers/project/project.service';
 
 type Status = '대기' | '중' | '완료';
 
@@ -13,9 +14,12 @@ export class ProjectDetailComponent implements OnInit {
   @Input()
   projects: Project[] = []
 
-  constructor() { }
+  constructor(
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit(): void {
+    this.projectService.getProject();
   }
 
 }
